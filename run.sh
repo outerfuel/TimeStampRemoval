9e788173bb92d475ec4fe2f48f5502957c8158be
zach <zschnepp@illinois.edu>
2023-05-02 13:31:33 -0500

    Updated for 3.2 Bash on MacOS

--- run.sh
+++ run.sh
@@ -14,8 +14,7 @@ timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 
 # Use sed to replace lines matching the pattern with an empty string
 # Then, use sed again to replace lines containing only numbers with an empty string
-# Finally, use awk to concatenate lines without a period, exclamation mark, or question mark at the end with the next line
-sed -e 's/^[0-9:,]\+ --> [0-9:,]\+$//g' -e '/^[0-9]*$/d' transcript.txt | awk '{if (substr($0,length($0)) == "." || substr($0,length($0)) == "!" || substr($0,length($0)) == "?") {if (NR!=1) print hold; print $0; hold=""} else {hold=hold" "$0}} END {if (hold!="") print hold}' > output/output_${timestamp}.txt
+sed -e 's/^[0-9:,]\{1,\} --> [0-9:,]\{1,\}$//g' -e '/^[0-9]*$/d' transcript.txt > output/output_${timestamp}.txt
 
 # Check if any modifications were made to the transcript file
 if cmp -s transcript.txt output/output_${timestamp}.txt; then
@@ -24,4 +23,4 @@ if cmp -s transcript.txt output/output_${timestamp}.txt; then
 fi
 
 # Display a success message with the full path of the output file on a new line
-echo "Transcript cleaned up successfully."$'\n'"Output file: $(realpath output/output_${timestamp}.txt)"
+echo -e "Transcript cleaned up successfully.\nOutput file: $(pwd)/output/output_${timestamp}.txt"
